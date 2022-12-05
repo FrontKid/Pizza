00c997786b21505376cacacbcc209e5b81ebc56f
Denys Poltorak <itwfrontend@gmail.com>
2022-12-05 13:02:34 +0200

    home axios  was changed on redux asynk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { v4 as getRandomKey } from 'uuid'
 import qs from 'qs'
 import { useSelector, useDispatch } from 'react-redux'
-import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { setCategoryId, setPageCount, setFilters } from '../redux/slices/filterSlice';
 import Catigories from '../components/Content/Catigories';
@@ -10,30 +9,26 @@ import Sort, { popUpList } from '../components/Content/Sort';
 import PizzaBlock from '../components/Content/PizzaBlock/PizzaBlock';
 import Placeholder from '../components/Content/PizzaBlock/Placeholder';
 import Pagination from '../components/Pagination/Pagination';
+import { fetchPizzas } from '../redux/slices/pizzaSlice';
 const Home = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const [pizzas, setPizzas] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
   const isSearch = useRef(false)
   const isMounted = useRef(false)
+
+  const { pizzas, status } = useSelector(state => state.pizzaSlice)
   const { categoryId, sort, pageCount } = useSelector(state => state.filterReducer)
   const { searchValue } = useSelector(state => state.searchSlice)
-  const fetchPizza = () => {
-    const order = sort.sort.includes('-') ? 'asc' : 'desc'
-    const sortBy = sort.sort.replace('-', '')
-    setIsLoading(true)
 
-    axios.get(`https://637a06387419b414df9821a1.mockapi.io/items?page=${pageCount}&limit=4&${categoryId
-      ? `category=${categoryId}`
-      : ''}&sortBy=${sortBy}&order=${order}&${searchValue
-        ? `search=${searchValue}`
-        : ''}`)
-      .then(res => {
-        setPizzas(res.data)
-        setIsLoading(false)
-      })
+  const getPizzas = async () => {
+    dispatch(fetchPizzas({
+      sort,
+      pageCount,
+      categoryId,
+      searchValue
+    }))
   }
+
   useEffect(() => {
     if (window.location.search) {
       const params = qs.parse(window.location.search.substring(1))
@@ -49,7 +44,7 @@ const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0)
     if (!isSearch.current) {
-      fetchPizza();
+      getPizzas();
     }
     isSearch.current = false
 
@@ -81,13 +76,21 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {isLoading
-          ? [...new Array(10)].map(() => <Placeholder key={getRandomKey()} />)
-          : pizzas
-            .map((pizza) => <PizzaBlock key={getRandomKey()} {...pizza} />)
+
+        {
+          status === 'error' ? <div className='content__error-info'>
+            <h2>Произошла ошибка 😕</h2>
+            <p>
+              К сожалению не удалось загрузить питсы =(
+              Повторите попытку позже.
+            </p>
+          </div> : status === 'loading'
+            ? [...new Array(10)].map(() => <Placeholder key={getRandomKey()} />)
+            : pizzas
+              .map((pizza) => <PizzaBlock key={getRandomKey()} {...pizza} />)
         }
       </div>
-      <Pagination onChangePage={onChangePage} />
+      {status === 'error' ? '' : <Pagination onChangePage={onChangePage} />}
     </div>
   )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from "./slices/filterSlice";
 import searchSlice from "./slices/searchSlice";
 import cartSlice from "./slices/cartSlice";
+import pizzaSlice from "./slices/pizzaSlice";
 export const store = configureStore({
   reducer: {
     filterReducer,
     searchSlice,
     cartSlice,
+    pizzaSlice,
   },
 })
